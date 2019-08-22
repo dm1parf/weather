3ee5873615092d792a89fc875eeff41d16d89058
dm1parf <dm1parf@gmail.com>
2019-08-22 21:44:18 +0300

    Updates

---
+++ precache-manifest.bfefcac233111f019cb54e959728f878.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd4cc2da4a7c637c62e1e4f5c21da964",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "892551e78a4aba33a374",
+    "url": "/weather/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "905564c0b626d240311b",
+    "url": "/weather/static/css/main.9e019bcd.chunk.css"
+  },
+  {
+    "revision": "892551e78a4aba33a374",
+    "url": "/weather/static/js/2.8a275d4a.chunk.js"
+  },
+  {
+    "revision": "905564c0b626d240311b",
+    "url": "/weather/static/js/main.78f34776.chunk.js"
+  },
+  {
+    "revision": "eff689c9325ca93fe6c0",
+    "url": "/weather/static/js/runtime~main.9cdddd60.js"
+  },
+  {
+    "revision": "cc54c19b29890841267ee0750eb25a00",
+    "url": "/weather/static/media/polygon.cc54c19b.jpg"
+  }
+]);
